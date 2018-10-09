5dd91a67d28e77e4519f4268fcea85c146447cc1
owen <headstupid2@gmail.com>
2018-10-08 22:11:41 -0500

    Zorin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello World!");
         System.out.println("Hello Chandler!");
         System.out.println("Hello Isaac!");
         System.out.println("This Github thing is pretty great!");
+        System.out.println("Testing ZorinOS");
     }
 }
